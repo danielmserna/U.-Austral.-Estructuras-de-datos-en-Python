6b5d62b175f6be8a4cd3e4530aec33eb059be9d0
Daniel Serna <35707534+danielmserna@users.noreply.github.com>
2020-11-05 09:41:04 -0500

    Add files via upload

---
+++ TrabajandoConTuplas.py
@@ -0,0 +1,21 @@
+tupla = (1, 2.5, 'Hola')
+
+tupla[0]
+tupla[1]
+tupla[2]
+
+tupla[:2]
+
+#Tupla vacía.
+tupla_vacia_1 = ()
+tupla_vacia_2 = tuple()
+
+#Tupla de un elemento
+tupla_2 = (5,)
+numero = (5)
+
+#Son inmutables
+tupla[2] = 3
+
+#Longitud de una tupla
+len(tupla)

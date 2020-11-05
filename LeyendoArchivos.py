603f9a1528269a27046d5cff3473630e4133311c
Daniel Serna <35707534+danielmserna@users.noreply.github.com>
2020-11-05 09:31:39 -0500

    Add files via upload

---
+++ LeyendoArchivos.py
@@ -0,0 +1,16 @@
+with open('c:/Users/Natalia/Desktop/CourseraPython/2/archivo.txt','r') as a_file:
+    print(a_file.read())
+
+with open('c:/Users/Natalia/Desktop/CourseraPython/2/archivo.txt','r') as a_file:
+    print(a_file.readline())
+
+with open('c:/Users/Natalia/Desktop/CourseraPython/2/archivo.txt','r') as a_file:
+    print(a_file.readlines())
+
+with open('c:/Users/Natalia/Desktop/CourseraPython/2/archivo.txt','r') as a_file:
+    print(list(a_file))
+
+with open('c:/Users/Natalia/Desktop/CourseraPython/2/archivo.txt','r') as a_file:
+    for line in a_file:
+        a_file.read()
+
